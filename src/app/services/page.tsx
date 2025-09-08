19916ec4038ab874d758edde89c4763d59c53b71
mrmartin <theophilusmartin123@gmail.com>
2025-09-08 16:05:47 -0500

    Created service header

---
+++ src/app/services/page.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+
+const ServicesPage = () => {
+  return (
+    <main className="mt-32">
+      <section className="grid grid-col-1 md:grid-cols-3 gap-5 w-full max-w-7xl mx-auto items-center">
+        <div className="flex flex-col gap-2 w-full">
+          <div className="w-full h-[3px] bg-yellow-500" />
+          <div className="w-full h-px bg-cyan-500" />
+          <div className="w-full h-[3px] bg-yellow-500" />
+        </div>
+        <h1 className="bg-clip-text font-sans text-transparent bg-gradient-to-t from-cyan-500 to-white text-xl md:text-[30px] lg:text-[40px] text-center">
+          Spade Wash Services
+        </h1>
+        <div className="flex flex-col gap-2 w-full">
+          <div className="w-full h-[3px] bg-yellow-500" />
+          <div className="w-full h-px bg-cyan-500" />
+          <div className="w-full h-[3px] bg-yellow-500" />
+        </div>
+      </section>
+
+      <div className="h-16 w-full flex items-center justify-center from-cyan-900 via-blue-950 to-transparent bg-conic-330 hue-rotate-90">
+        <h2 className="text-center text-lg md:text-xl font-thin tracking-wide font-mono uppercase">
+          Come Ace your Car with Spade Car Wash
+        </h2>
+      </div>
+
+      <section className="mt-20">
+        <div className="flex md:flex-row flex-col items-center justify-center text-center py-10 ">
+          <h2 className="max-w-sm md:text-lg font-sans py-4 text-left text-gray-400">
+           FInd as down the block to keep your car squicky clean. They say we&apos;re popular than Uber in the hood
+          </h2>
+          <div className="w-full max-w-4xl flex-1">
+            <form className="relative flex items-center">
+              <input
+                type="search"
+                name="search"
+                id="search"
+                placeholder="Search..."
+                className="w-full h-12 rounded-full pl-5 pr-28 bg-white/10 backdrop-blur-md border border-white/20 text-white placeholder-gray-300 focus:outline-none focus:ring-2 focus:ring-white/50 shadow-sm"
+              />
+              <button
+                type="submit"
+                className="font-semibold absolute right-2 top-1/2 -translate-y-1/2 h-10 px-6 rounded-full bg-gradient-to-r from-yellow-500 to-neutral-300 text-gray-900 shadow-md hover:shadow-lg hover:scale-105 transition-all duration-300"
+              >
+                Search
+              </button>
+            </form>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
+};
+
+export default ServicesPage;
---
+++ src/app/services/services.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Services = () => {
+  return (
+    <section className='grid grid-cols-1 md:grid-cols-4'>
+
+    </section>
+  )
+}
+
+export default Services
